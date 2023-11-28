e279cdcc594fe1f31e0caa42bdf921d233b59108
Programing Hero <sajalb1899@gmail.com>
2023-11-29 00:01:18 +0600

    admin asset list page added

--- src/Page/AdminPage/AddAsset/AddAsset.jsx
+++ src/Page/AdminPage/AddAsset/AddAsset.jsx
@@ -1,13 +1,26 @@
+import { useContext } from "react";
+import useAxios from "../../../Component/Hooks/useAxios";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const AddAsset = () => {
-    const handleSubmit = e =>{
+    const { user } = useContext(AuthContext)
+    const axiosApi = useAxios();
+    const date = new Date();
+    
+    const handleSubmit = e => {
         e.preventDefault();
         const form = e.target;
         const productName = form.productName.value;
         const productType = form.productType.value;
         const productQuantity = form.productQuantity.value;
-        const asset = {productName,productType, productQuantity:parseInt(productQuantity)}
+        const asset = { productName, productType, productQuantity: parseInt(productQuantity), adminEmail: user.email, postDate:date }
         console.log(asset)
+        axiosApi.post('/assets', asset)
+            .then(res => {
+                console.log(res.data)
+            })
+            
+
     }
     return (
         <div className="lg:w-10/12 md:w-10/12 m-auto bg-slate-200 pb-40">
@@ -18,30 +31,30 @@ const AddAsset = () => {
 
             <form onSubmit={handleSubmit} className="card-body ">
                 <div className="shadow-current shadow px-2 rounded-md pb-10 pt-4">
-                <div className="grid md:grid-cols-3 gap-5 w-full">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-lg font-medium">Product Name</span>
-                        </label>
-                        <input type="text" placeholder="Product Name" name="productName" className="input input-bordered w-full" required />
+                    <div className="grid md:grid-cols-3 gap-5 w-full">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-lg font-medium">Product Name</span>
+                            </label>
+                            <input type="text" placeholder="Product Name" name="productName" className="input input-bordered w-full" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-lg font-medium">Product Type</span>
+                            </label>
+                            <input type="text" placeholder="Product Type" name="productType" className="input input-bordered w-full" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-lg font-medium">Product Quantity</span>
+                            </label>
+                            <input type="number" placeholder="Product Quantity" name="productQuantity" className="input input-bordered w-full" required />
+
+                        </div>
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-lg font-medium">Product Type</span>
-                        </label>
-                        <input type="text" placeholder="Product Type" name="productType" className="input input-bordered w-full" required />
+                    <div className="form-control mt-6">
+                        <button className="btn text-lg font-medium btn-primary bg-slate-500 text-white">Add Asset</button>
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-lg font-medium">Product Quantity</span>
-                        </label>
-                        <input type="number" placeholder="Product Quantity" name="productQuantity" className="input input-bordered w-full" required />
-                        
-                    </div>
-                </div>
-                <div className="form-control mt-6">
-                    <button className="btn text-lg font-medium btn-primary bg-slate-500 text-white">Add Asset</button>
-                </div>
                 </div>
             </form>
         </div>
